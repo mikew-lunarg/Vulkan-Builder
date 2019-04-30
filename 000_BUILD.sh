40c8f2f6eec235d0b1d449407af71fcc37b7d7f4
Mike Weiblen <mikew@lunarg.com>
2019-04-29 18:23:40 -0600

    write info to $DESC_FILE

--- 000_BUILD.sh
+++ 000_BUILD.sh
@@ -11,6 +11,7 @@ INSTALL_DIR="${HOME}/VK_INSTALL"
 [ -e "$INSTALL_DIR" ] && ( echo "must delete $INSTALL_DIR first"; exit 1 )
 mkdir -p "$INSTALL_DIR"
 
+DESC_FILE="$INSTALL_DIR/repo_descriptions.txt"
 
 describe_repo() {
     pwd -P
@@ -25,16 +26,16 @@ build_repo() {
     echo
     echo "BUILD $(pwd -P) ========================================================"
     echo
-    describe_repo >> "$INSTALL_DIR/source.txt"
+    describe_repo >> "$DESC_FILE"
     rm -rf BUILD/
     time ./000_BUILD.sh "$INSTALL_DIR"
 }
 
 
-echo "START ${BASH_SOURCE[0]}" >> "$INSTALL_DIR/source.txt"
-date >> "$INSTALL_DIR/source.txt"
-echo >> "$INSTALL_DIR/source.txt"
-describe_repo >> "$INSTALL_DIR/source.txt"
+echo "START ${BASH_SOURCE[0]}" >> "$DESC_FILE"
+date >> "$DESC_FILE"
+echo >> "$DESC_FILE"
+describe_repo >> "$DESC_FILE"
 
 build_repo "$HOME/gits/github.com/KhronosGroup/glslang"
 build_repo "$HOME/gits/github.com/KhronosGroup/Vulkan-Headers"
@@ -43,7 +44,7 @@ build_repo "$HOME/gits/github.com/KhronosGroup/Vulkan-Tools"
 build_repo "$HOME/gits/github.com/KhronosGroup/Vulkan-ValidationLayers"
 build_repo "$HOME/gits/github.com/LunarG/VulkanTools"
 
-echo "FINISH ${BASH_SOURCE[0]}" >> "$INSTALL_DIR/source.txt"
-date >> "$INSTALL_DIR/source.txt"
+echo "FINISH ${BASH_SOURCE[0]}" >> "$DESC_FILE"
+date >> "$DESC_FILE"
 
 # vim: set et sw=4 ts=8 ic ai:
