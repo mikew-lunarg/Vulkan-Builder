b150126f6370f6ff7c42581b6695141f476dd24b
Mike Weiblen <mikew@lunarg.com>
2019-05-07 14:31:48 -0600

    000_BUILD.sh

--- 000_BUILD.sh
+++ 000_BUILD.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 # http://mew.cx/ 2019-03-20
 
-#set -o errexit
-#set -o nounset
-#set -o physical
+set -o nounset
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+START_DIR="$(pwd -P)"
 
 INSTALL_DIR="${HOME}/VK_INSTALL"
-[ -e "$INSTALL_DIR" ] && ( echo "must delete $INSTALL_DIR first"; exit 1 )
+[ ! -e "$INSTALL_DIR" ] || { echo "you must delete $INSTALL_DIR first"; exit 1; }
 mkdir -p "$INSTALL_DIR"
 
 DESC_FILE="$INSTALL_DIR/repo_descriptions.txt"
@@ -21,28 +20,26 @@ describe_repo() {
 }
 
 build_repo() {
-    cd "$1"
+    cd "/home/mikew/gits/github.com/${1}/${2}"
+    cp "${START_DIR}/BUILD_${1}_${2}.sh" "."
     echo -e "\n\n\nBUILD $(pwd -P) ========================================================\n"
     describe_repo >> "$DESC_FILE"
     rm -rf BUILD/
     time ./000_BUILD.sh "$INSTALL_DIR"
 }
 
+# Make it so ################################################################
 
-echo "START ${BASH_SOURCE[0]}" >> "$DESC_FILE"
-date >> "$DESC_FILE"
-echo >> "$DESC_FILE"
-describe_repo >> "$DESC_FILE"
+(echo "START ${BASH_SOURCE[0]}"; date; echo; describe_repo) >> "$DESC_FILE"
 
-build_repo "$HOME/gits/github.com/KhronosGroup/glslang"
-build_repo "$HOME/gits/github.com/KhronosGroup/Vulkan-Headers"
-build_repo "$HOME/gits/github.com/KhronosGroup/Vulkan-Loader"
-build_repo "$HOME/gits/github.com/KhronosGroup/Vulkan-Tools"
-build_repo "$HOME/gits/github.com/KhronosGroup/Vulkan-ValidationLayers"
-build_repo "$HOME/gits/github.com/LunarG/VulkanTools"
-#build_repo "$HOME/gits/github.com/LunarG/VulkanSamples"
+build_repo KhronosGroup glslang
+build_repo KhronosGroup Vulkan-Headers
+build_repo KhronosGroup Vulkan-Loader
+build_repo KhronosGroup Vulkan-Tools
+build_repo KhronosGroup Vulkan-ValidationLayers
+build_repo LunarG VulkanTools
+#build_repo LunarG VulkanSamples
 
-echo "FINISH ${BASH_SOURCE[0]}" >> "$DESC_FILE"
-date >> "$DESC_FILE"
+(echo "FINISH ${BASH_SOURCE[0]}"; date) >> "$DESC_FILE"
 
 # vim: set et sw=4 ts=8 ic ai:
