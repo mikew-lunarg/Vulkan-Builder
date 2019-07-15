2efda671dee0ceea784f09a9390a84325b620ed0
Mike Weiblen <mikew@lunarg.com>
2019-07-15 12:01:16 -0600

    move gav from ~/bin

---
+++ gav.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# Execute a command in each of the listed Vulkan GitHub repos.
+# http://mew.cx/
+
+
+for i in \
+    KhronosGroup/glslang \
+    KhronosGroup/Vulkan-Headers \
+    KhronosGroup/Vulkan-Loader \
+    KhronosGroup/Vulkan-Tools \
+    KhronosGroup/Vulkan-ValidationLayers \
+    LunarG/VulkanTools \
+    LunarG/VulkanSamples
+do
+    cd "/home/mikew/gits/github.com/$i"
+    echo -e "\n{ \"$i\""
+    eval "$@"
+    echo -e "} \$?=$?\n"
+done
+
+# vim: set sw=4 ts=8 et ic ai:
