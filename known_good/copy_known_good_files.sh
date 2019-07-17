034e8c531211415ce61bc8f32389f1d663d53c90
Mike Weiblen <mikew@lunarg.com>
2019-07-17 15:55:01 -0600

    rewrite copy_known_good_files.sh to use symlinks

--- known_good/copy_known_good_files.sh
+++ known_good/copy_known_good_files.sh
@@ -1,10 +1,3 @@
 #! /bin/bash -x
 
-( cd ~/gh; tar cvf - \
-    KhronosGroup/glslang/known_good.json \
-    KhronosGroup/Vulkan-Loader/scripts/known_good.json \
-    KhronosGroup/Vulkan-Tools/scripts/known_good.json \
-    KhronosGroup/Vulkan-ValidationLayers/scripts/known_good.json \
-    LunarG/VulkanTools/scripts/known_good.json \
-    LunarG/VulkanSamples/scripts/known_good.json \
-) | tar xf -
+more *.json | cat
