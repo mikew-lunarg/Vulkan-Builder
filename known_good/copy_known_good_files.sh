9ce456bdb507c1f52f116c0bb203e9520f381664
Mike Weiblen <mikew@lunarg.com>
2019-08-28 20:38:55 -0600

    overhaul copy_known_good_files.sh

--- known_good/copy_known_good_files.sh
+++ known_good/copy_known_good_files.sh
@@ -1,3 +1,17 @@
-#! /bin/bash -x
+#! /bin/bash
 
-more *.json | cat
+cd ~/gh
+for i in \
+    KhronosGroup/Vulkan-Headers/scripts/known_good.json \
+    KhronosGroup/Vulkan-Loader/scripts/known_good.json \
+    KhronosGroup/Vulkan-Tools/scripts/known_good.json \
+    KhronosGroup/Vulkan-ValidationLayers/scripts/known_good.json \
+    LunarG/VulkanSamples/scripts/known_good.json \
+    LunarG/VulkanTools/scripts/known_good.json
+do
+    echo -e "\t$i"
+    jq -M '.repos[] | "\(.name) \(.commit)"' < "$i"
+    echo
+done
+
+# vim: set sw=4 ts=8 et ic ai:
